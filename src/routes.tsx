889e338da332757d553e0a42dcc8a05e3ae5ea1b
dotcomstar <gamestar109@live.com>
2023-09-15 15:55:15 -0700

    Clean up comment

--- src/routes.tsx
+++ src/routes.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> }, // Default component
-      { path: "/projects", element: <ProjectsPage /> }, // Default component
+      { path: "/projects", element: <ProjectsPage /> },
     ],
   },
 ]);

671d5b9e4755755f8b5a34d23061e05e34114f77
dotcomstar <gamestar109@live.com>
2023-10-04 13:44:01 -0700

    Enhance projects cards

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,16 +1,26 @@
 export default [
   {
-    name: "Botball Robotics",
+    name: "ScheduleTerp Website",
     description:
-      "Watch our globals run! https://www.youtube.com/playlist?list=PLFBMxG1YHu_eViSOG3yIg4HtCXumexXUV",
-    shortDescription:
-      "Placed 10th globally at a fully-autonomous high school robotics competition",
-    image: "https://img.youtube.com/vi/ZzfZ50dQAYE/sddefault.jpg",
-    slug: "botball",
-    url: "https://github.com/rockvillerobotics/Incredibots2018",
-    startDate: "2015",
-    endDate: "2018",
-    isCurrent: false,
+      "A website to auto-generate the best course schedule for UMD students. Just input your courses and click generate!",
+    shortDescription: "Find the best schedule for UMD Courses",
+    image: "/src/assets/ScheduleTerpWebsite.png",
+    slug: "scheduleterp",
+    url: "https://github.com/dotcomstar/ScheduleTerpFrontend",
+    startDate: "2023",
+    endDate: "2023",
+    isCurrent: true,
+  },
+  {
+    name: "GameHub Website",
+    description: "A website to find new games to play!",
+    shortDescription: "Find new games to play!",
+    image: "/src/assets/GameHubWebsite.png",
+    slug: "gamehub",
+    url: "https://github.com/dotcomstar/game-hub",
+    startDate: "2023",
+    endDate: "2023",
+    isCurrent: true,
   },
   {
     name: "Formula E Dashboard",
@@ -22,6 +32,19 @@ export default [
     url: "",
     startDate: "2022",
     endDate: "2022",
+    isCurrent: false,
+  },
+  {
+    name: "Botball Robotics",
+    description:
+      "Watch our globals run! https://www.youtube.com/playlist?list=PLFBMxG1YHu_eViSOG3yIg4HtCXumexXUV",
+    shortDescription:
+      "Placed 10th globally at a fully-autonomous high school robotics competition",
+    image: "https://img.youtube.com/vi/ZzfZ50dQAYE/sddefault.jpg",
+    slug: "botball",
+    url: "https://github.com/rockvillerobotics/Incredibots2018",
+    startDate: "2015",
+    endDate: "2018",
     isCurrent: false,
   },
 ];
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,35 +1,53 @@
-import { Heading, LinkBox, LinkOverlay, Text, Image } from "@chakra-ui/react";
-import useData from "../hooks/useData";
+import {
+  GridItem,
+  Heading,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
 import projects from "../data/projects";
 import Project from "../entities/Project";
+import useData from "../hooks/useData";
 
 const ProjectsPage = () => {
   const { data } = useData<Project>(projects);
 
   return (
-    <>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      margin={1}
+      marginTop={2}
+    >
       {data.map((project) => (
-        <LinkBox
-          key={project.slug}
-          borderWidth="1px"
-          rounded="md"
-          maxW="sm"
-          p="5"
-        >
-          <Heading size="md" my="2">
-            <LinkOverlay
-              href={project.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {project.name}
-            </LinkOverlay>
-          </Heading>
-          <Text>{project.shortDescription}</Text>
-          <Image src={project.image} />
-        </LinkBox>
+        <GridItem>
+          <LinkBox
+            key={project.slug}
+            _hover={{
+              transform: "scale(1.03)",
+              transition: "transform .15s ease-in",
+            }}
+            borderWidth="1px"
+            borderRadius={10}
+            p="5"
+          >
+            <Heading size="md" my="2">
+              <LinkOverlay
+                href={project.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {project.name}
+              </LinkOverlay>
+            </Heading>
+            <Text>{project.shortDescription}</Text>
+            <Image src={project.image} />
+          </LinkBox>
+        </GridItem>
       ))}
-    </>
+    </SimpleGrid>
   );
 };
 

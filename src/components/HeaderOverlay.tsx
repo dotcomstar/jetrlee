35e772370f09b6c8cc67f2ead63888099988051b
dotcomstar <gamestar109@live.com>
2023-10-13 14:49:04 -0700

    Remove unused commit

--- src/components/HeaderOverlay.tsx
+++ src/components/HeaderOverlay.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Container, Text } from "@chakra-ui/react";
+import { Box, Center, Container } from "@chakra-ui/react";
 import jetPlanefield from "../assets/jet-planefield.jpg";
 import CloudTextDynamic from "./CloudTextDynamic";
 import CloudText2 from "./CloudText2";
